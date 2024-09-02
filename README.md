e3338a1b19c53f7054ea57521307a5ab9e1e74a2
Walikuperek <56729104+Walikuperek@users.noreply.github.com>
2024-09-02 22:32:34 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# API-clients
-API clients for Node and Browser(based on this account API repositories).
+# API Clients for Node and Browser
+
+*Based on API's from [GitHub QUAK repositories](https://github.com/walikuperek).*
+
+Client example usage:
+```javascript copy
+// Browser
+const client = NewsletterAPIClient(fetch)
+await client.subscribers.addNewSubscriber('email@em.com', 'Full Name') // will send confirmation email
+
+// NodeJS
+const fetch = require('node-fetch')
+const client = NewsletterAPIClient(fetch)
+const jwt = await client.auth.login('admin', 'password')
+await client.subscribers.sendNewsletterToAuthorizedSubscribers(jwt, 'subject', '1st newsletter message 🎉🎂')
+```
+
+Simple install into your project:
+```bash copy
+cd /path/to/repository
+git submodule add https://github.com/Walikuperek/API-clients.git libs/api-client
+git add .gitmodules libs/api-client
+git commit -m "Added API clients"
+```
+
+## Clients
+
+- **[NewsletterAPI](https://github.com/Walikuperek/NewsletterAPI/tree/master)** - append new subscribers, send Newsletter, etc.
+
+<br />
+<hr />
+
+**In case you will struggle with Submodules:**
+
+> How to clone repo with submodules?
+> ```bash
+> git clone --recurse-submodules https://github.com/user/repo-with-submodules.git
+> ```
+>
+> Or, you already cloned without `--recurse-submodules`:
+>
+> ```bash
+> git submodule init
+> git submodule update
+> ```
+>
+> How to update submodule
+> ```
+> cd path/to/submodule
+> git pull origin main
+> cd path/to/repository
+> git add path/to/submodule
+> git commit -m "Submodule updated"
+> ```
+
+**Author**
+QUAK Kacper Walczak, [visit website to see more details](https://quak.com.pl)
---
+++ newsletter-api.client.js
@@ -0,0 +1,34 @@
+const Http = require('./lib/http');
+
+/**
+ * @param fetchFn for browser simply `fetch` | for node `const fetch = require('node-fetch')`
+ * @example Browser
+ *  const client = NewsletterAPIClient(fetch)
+ *  await client.subscribers.addNewSubscriber('email@em.com', 'Full Name')
+ * 
+ * @example NodeJS
+ *  const fetch = require('node-fetch')
+ *  const client = NewsletterAPIClient(fetch)
+ *  const jwt = await client.auth.login('admin', 'password')
+ *  await client.subscribers.sendNewsletterToAuthorizedSubscribers(jwt, 'subject', '1st newsletter message 🎉🎂')
+ */
+function NewsletterAPIClient(fetchFn) {
+    const http = Http(fetchFn);
+    return {
+        auth: {
+            login: async (username, password) => await http.post('/api/auth/login', {username, password})["token"],
+            signup: async (jwt, username, password) => await http.post('/api/auth/login', {username, password}, {'Authorization': jwt})
+        },
+        subscribers: {
+            authorizeSubscriber: async (token) => await http.get(`/api/subscribers/confirm/${token}`),
+            unsubscribeSubscriber: async (token) => await http.get(`/api/subscribers/unsubscribe/${token}`),
+            addNewSubscriber: async (email, name) => await http.post(`/api/subscribers/`, {email, name}),
+            getAuthorizedSubscribers: async (jwt) => await http.get(`/api/subscribers/`, {}, {'Authorization': jwt}),
+            deleteSubscriber: async (jwt, id) => await http.delete(`/api/subscribers/${id}`, {'Authorization': jwt}),
+            sendNewsletterToAuthorizedSubscribers: async (jwt, subject, message) =>
+                await http.post(`/api/subscribers/send`, {subject, message}, {'Authorization': jwt})
+        }
+    }
+}
+
+module.exports = NewsletterAPIClient;
